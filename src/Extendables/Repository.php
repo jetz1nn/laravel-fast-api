def76cd94e273e38b4bf83758c8d5c9cd57fa2a9
mathaus <mathaus.aquicob@gmail.com>
2022-03-12 21:17:47 -0300

    a

--- src/Extendables/Repository.php
+++ src/Extendables/Repository.php
@@ -47,6 +47,7 @@ abstract class Repository {
     }
 
     public function applyFilters(array $filters) {
+        dump($this->filter_map);
         foreach ($filters as $filter => $filter_value) {
 
             $filter_name = array_key_exists($filter, $this->filter_map) ? $this->filter_map[$filter] : $filter;
